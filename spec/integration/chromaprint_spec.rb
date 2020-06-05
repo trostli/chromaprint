29e980d5e636459af6fe7ed3d1223238e41d3299
trostli <daniel.trostli@gmail.com>
2020-06-05 14:44:59 -0700

    add additional test

--- spec/integration/chromaprint_spec.rb
+++ spec/integration/chromaprint_spec.rb
@@ -14,18 +14,29 @@ describe 'Chromaprint integration' do
     File.join(FIXTURES_PATH, filename)
   end
 
-  let(:rate)     { 22050 }
-  let(:channels) { 1     }
-
   it 'should compare fingerprints' do
     data_a = File.binread(fixture('a.wav'))
     data_b = File.binread(fixture('b.wav'))
+    rate = 2250
+    channels = 1
 
     chromaprint = Chromaprint::Context.new(rate, channels)
 
     fp_a = chromaprint.get_fingerprint(data_a)
     fp_b = chromaprint.get_fingerprint(data_b)
-
     fp_a.compare(fp_b).should be_within(0.005).of(0.995)
+  end
+
+  it 'should be able to tell if audio is similar but not identical' do
+    piano_a = File.binread(fixture('piano-looped-melody.wav'))
+    piano_b = File.binread(fixture('piano-looped-melody-thin.wav'))
+    rate = 44100
+    channels = 2
+
+    chromaprint = Chromaprint::Context.new(rate, channels)
+
+    fp_a = chromaprint.get_fingerprint(piano_a)
+    fp_b = chromaprint.get_fingerprint(piano_b)
+    fp_a.compare(fp_b).should be_within(0.005).of(0.874)
   end
 end
